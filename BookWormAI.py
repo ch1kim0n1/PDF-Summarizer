a92fbced6e18b0bef1f384f2a712ad1356156a94
ch1kim0n1 <chikimoni61@gmail.com>
2023-04-28 22:56:08 -0500

    update 1

--- BookWormAI.py
+++ BookWormAI.py
@@ -1,53 +1,56 @@
-import nltk
+import sys
 import nltk
 nltk.download('punkt')
-import tkinter as tk
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QFileDialog, QTextEdit
 
-class App(tk.Tk):
+
+class App(QWidget):
 
     def __init__(self):
         super().__init__()
 
-        # Create the main window
-        self.main_window = tk.Frame(self)
-        self.main_window.pack(side="top", fill="both", expand=True)
-
         # Create the upload button
-        self.upload_button = tk.Button(self.main_window, text="Upload PDF", command=self.upload_pdf)
-        self.upload_button.pack(side="left")
+        self.upload_button = QPushButton("Upload PDF", self)
+        self.upload_button.clicked.connect(self.upload_pdf)
+        self.upload_button.setGeometry(20, 20, 120, 30)
 
         # Create the URL input field
-        self.url_input = tk.Entry(self.main_window)
-        self.url_input.pack(side="left")
+        self.url_input = QLineEdit(self)
+        self.url_input.setGeometry(150, 20, 300, 30)
 
         # Create the analyze button
-        self.analyze_button = tk.Button(self.main_window, text="Analyze", command=self.analyze_pdf)
-        self.analyze_button.pack(side="left")
+        self.analyze_button = QPushButton("Analyze", self)
+        self.analyze_button.clicked.connect(self.analyze_pdf)
+        self.analyze_button.setGeometry(460, 20, 120, 30)
 
         # Create the output label
-        self.output_label = tk.Label(self.main_window, text="")
-        self.output_label.pack(side="left")
+        self.output_label = QTextEdit(self)
+        self.output_label.setGeometry(20, 70, 560, 300)
+        #self.output_label.setTextColor(Qt.red)
 
         # Create the error label
-        self.error_label = tk.Label(self.main_window, text="")
-        self.error_label.pack(side="left")
+        self.error_label = QLabel(self)
+        self.error_label.setGeometry(20, 380, 560, 20)
 
-        # Bind the event handlers
-        self.upload_button.bind("<Button-1>", self.upload_pdf)
-        self.analyze_button.bind("<Button-1>", self.analyze_pdf)
-
-    def upload_pdf(self, event):
+    def upload_pdf(self):
         # Get the file name
-        filename = tk.filedialog.askopenfilename(filetypes=[("PDF files", "*.pdf")])
+        filename, _ = QFileDialog.getOpenFileName(self, "Upload PDF", "", "PDF files (*.pdf)")
 
         # Check if the file is a PDF
         if not filename.endswith(".pdf"):
-            self.error_label.config(text="Please select a PDF file")
+            self.error_label.setText("Please select a PDF file")
             return
 
         # Read the PDF file
         with open(filename, "rb") as f:
-            pdf_text = f.read()
+            try:
+                pdf_text = f.read().decode("utf-8")
+            except UnicodeDecodeError:
+                self.error_label.setText("The file is not in UTF-8 encoding.")
+                return
+
+        # Set the URL input field to the selected file path
+        self.url_input.setText(filename)
 
         # Analyze the PDF file
         self.analyze_pdf(pdf_text)
@@ -60,12 +63,22 @@ class App(tk.Tk):
         named_entities = nltk.ne_chunk(sentences)
 
         # Print the named entities
+        entities_str = ""
         for entity in named_entities:
-            self.output_label.config(text=self.output_label.cget("text") + entity + "\n")
+            entities_str += str(entity) + "\n"
+        self.output_label.setText(entities_str)
 
-    def main(self):
-        self.mainloop()
+        # Print out "PDF is uploaded successfully"
+        self.output_label.appendHtml("<p style='color:red;'>PDF is uploaded successfully</p>")
+
+    def run(self):
+        self.setGeometry(100, 100, 600, 420)
+        self.setWindowTitle("PDF Analyzer")
+        self.show()
+
 
 if __name__ == "__main__":
-    app = App()
-    app.main()
+    app = QApplication(sys.argv)
+    ex = App()
+    ex.run()
+    sys.exit(app.exec_())
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # BookWormAI
+
+Making a bot that will read each book for you and them give a summary!
